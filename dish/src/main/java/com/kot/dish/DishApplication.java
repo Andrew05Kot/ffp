c3ff259a5774c5233e2afd4d144d21c7c166475a
Andriy Kotiyga <kotygaandrey05@gmail.com>
2022-07-11 15:06:43 +0300

    initialized first microservice for working with Dishes

---
+++ dish/src/main/java/com/kot/dish/DishApplication.java
@@ -0,0 +1,13 @@
+package com.kot.dish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DishApplication.class, args);
+	}
+
+}
---
+++ dish/src/test/java/com/kot/dish/DishApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kot.dish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DishApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
