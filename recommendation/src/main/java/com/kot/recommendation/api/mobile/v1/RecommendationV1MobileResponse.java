82c31f51892ab947cf20c735cdf25fb5c3d129b8
Andrew05Kot <kotygaandrey05@gmail.com>
2023-12-04 19:52:59 +0200

    * init mock recommendation system microservice

---
+++ recommendation/src/main/java/com/kot/recommendation/api/mobile/v1/RecommendationV1MobileResponse.java
@@ -0,0 +1,12 @@
+package com.kot.recommendation.api.mobile.v1;
+
+import java.util.Set;
+import lombok.Data;
+
+@Data
+public class RecommendationV1MobileResponse {
+
+    private String userId;
+    private Set<String> recommendedDishesIds;
+
+}
